781dce46158e48ab2145eb424f7b093542c194f1
eseoseodion <ehodion1@sheffield.ac.uk>
2023-10-02 08:07:10 +0100

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+FILEPATH = "todos_item.txt" #constant
+
+def get_todos(filepath=FILEPATH): #default argument
+    """ Read a text file and return the list 
+    of to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH): #default arguments come after non-default arguments
+    """Write the to-do items list 
+    in the text file.
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+#this function retuns nothing
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
---
+++ gui.py
@@ -0,0 +1,13 @@
+import functions
+import  PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do") #Text is not a function but a type assocaited with pysimplegu
+input_box = sg.InputText(tooltip="Enter todo") #InputText is not a function but a type assocaited with pysimplegu
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-DO App', layout=[[label], [input_box, add_button]]) #Window is not a function but a type assocaited with pysimplegui, inside the bracket and quotes is an instance of window
+window.read() #read is a method i.e. like a list with .upper(), .capitalize() etc. It displays the window
+window.close()
+
+
+
